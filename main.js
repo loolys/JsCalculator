b759d7ad0ceefd6ef2c85ecc5a654fb8f5566f17
loolys <pierre.liljekvist@outlook.com>
2016-12-01 13:16:03 +0100

    Added code to main.js for simple calculations in the console.

--- main.js
+++ main.js
@@ -1,3 +1,70 @@
 /**
  * Created by Pierre on 2016-12-01.
  */
+
+var getNumber, setNumber;
+var getAttr, setAttr;
+
+(function(){
+    /*
+    Initializes the getter and setter for
+    number when the code is first ran.
+     */
+    var number = 0;
+
+    getNumber = function(){
+        return number;
+    };
+
+    setNumber = function(num){
+        if (typeof num === "number"){
+            number = num;
+        } else{
+            console.log("Not a valid number");
+        }
+    };
+}());
+
+
+(function(){
+
+    var attr = "";
+
+    getAttr = function(){
+        return attr;
+    };
+
+    setAttr = function(at){
+        if (at == "+"){
+            attr = "+";
+        }
+        else if(at == "-"){
+            attr = "-";
+        }
+        else if(at == "*"){
+            attr = "*";
+        }
+        else if(at == "/"){
+            attr = "/";
+        }
+    };
+
+}());
+
+function calc(newNum){
+    var number = getNumber();
+    setNumber(newNum);
+    var currentAttr = getAttr();
+
+    if(currentAttr == "+"){
+        number += getNumber();
+    } else if(currentAttr == "-"){
+        number -= getNumber();
+    } else if(currentAttr == "*"){
+        number *= getNumber();
+    } else if(currentAttr == "/"){
+        number /= getNumber();
+    }
+    setNumber(number);
+    return number;
+}
